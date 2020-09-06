d1f4e20b2f1915e1ffc06fc9bc16c3b6bbac4bdd
duho3abp <DanZhidkof@yandex.ru>
2020-09-06 16:49:54 +0300

    add recurcive method

--- decision.js
+++ decision.js
@@ -1,25 +1,52 @@
 'use strict';
 
+// ---------------------RECURSIVE-METHOD---------------------
+
 const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
 let newArr = [];
 
-arr.forEach(a => {
-    arr.forEach(b => {
-        arr.forEach(c => {
-            arr.forEach(d => {
-                arr.forEach(e => {
-                    arr.forEach(f => {
-                        newArr = [...newArr, `${a}-${b}-${c}-${d}-${e}-${f}`];    
-                    });
-                    newArr = [...newArr, `${a}-${b}-${c}-${d}-${e}`];
-                });
-                newArr = [...newArr, `${a}-${b}-${c}-${d}`];
+const iteratingArray = (array, item) => {
+    if (item) {
+        if (+item.replace(/\-/g, '').length < arr.length) {
+            array.forEach(el => {
+                const p = `${item}-${el}`;
+                newArr = [...newArr, p];
+                iteratingArray(arr, p);
             });
-            newArr = [...newArr, `${a}-${b}-${c}`];
+        } else { return; }
+    } else {
+        array.forEach(el => {
+            newArr = [...newArr, el];
+            iteratingArray(arr, el);
         });
-        newArr = [...newArr, `${a}-${b}`];
-    });
-    newArr = [...newArr, a];
-});
+    }
+};
+iteratingArray(arr);
 
 console.log(newArr);
+
+// ---------------------NOT-RECURSIVE-METHOD---------------------
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// let newArr = [];
+
+// arr.forEach(a => {
+//     arr.forEach(b => {
+//         arr.forEach(c => {
+//             arr.forEach(d => {
+//                 arr.forEach(e => {
+//                     arr.forEach(f => {
+//                         newArr = [...newArr, `${a}-${b}-${c}-${d}-${e}-${f}`];    
+//                     });
+//                     newArr = [...newArr, `${a}-${b}-${c}-${d}-${e}`];
+//                 });
+//                 newArr = [...newArr, `${a}-${b}-${c}-${d}`];
+//             });
+//             newArr = [...newArr, `${a}-${b}-${c}`];
+//         });
+//         newArr = [...newArr, `${a}-${b}`];
+//     });
+//     newArr = [...newArr, a];
+// });
+
+// console.log(newArr);
