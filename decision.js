5df72581b12d8fdad72856383d5ed26e6a779859
duho3abp <DanZhidkof@yandex.ru>
2020-09-08 03:40:48 +0300

    solving a problem using a recursive method

--- decision.js
+++ decision.js
@@ -1,12 +1,24 @@
 'use strict';
 
-const arr = ['a', 'b', 'c', 'd', 'e', 'f'],
+// ---------------------RECURSIVE-METHOD---------------------
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'],
+const arr = ['a', 'b', 'f', 'i', 'w'],
       arrLength = arr.length;
-let newArr = [], skip;
+let newArr = [],
+    i = 0,
+    pass = false,
+    repeat = false;
 
 const replaceString = (str) => str.replace(/\-/g, '');
 
-const testArr = (array) => {
+const passTest = (ar, it) => {
+    for (let i = 0; i < ar.length; i++) {
+        if (ar[i] === it) { pass = true; }
+    }
+};
+
+const arrTest = (array) => {
     const clearArr = replaceString(array);
     let newArray = [],
         test = '';
@@ -18,7 +30,7 @@ const testArr = (array) => {
             }
         }    
     });
-
+    
     newArray = newArray.filter(itm => itm);
     if (newArray.length > 1) {
         newArray.forEach(w => test += w);
@@ -27,129 +39,41 @@ const testArr = (array) => {
     return test;
 };
 
-const iteratingArray = ({array, item, l}) => {
-    if (item) {
-        const str = replaceString(item);
+const iteratingArray = (array, str) => {
+    if (str) {
+        if (replaceString(str).length < arrLength) {
+            array.forEach(item => {
+                if (str !== item) {
+                    passTest(str, item);
 
-        if (+str.length < l) {
-            array.forEach(el => {
+                    if (!pass) {
+                        const p = `${str}-${item}`;
+                        arrTest(p);
+                        newArr.forEach(itm => { if (replaceString(itm) === arrTest(p)) { repeat = true; } });
 
-                if (str.length > 1) {
-                    for (let i = 0; i < str.length; i++) {
-                        if (str[i] === el) {
-                            return;
+                        if (!repeat) {
+                            newArr[i] = p;
+                            i++;
+                            pass = false;
+                            repeat =false;
+                            iteratingArray(arr, p);
                         }
+
+                        repeat =false;
                     }
+
+                    pass = false;
                 }
-
-                if (item !== el) {
-                    const p = `${item}-${el}`,
-                          testP = testArr(p);
-
-                    newArr.forEach(element => {
-                        if (replaceString(element) == testP) { skip = true; }
-                    });
-
-                    if (!skip) {
-                        newArr = [...newArr, p];
-                        skip = false;
-
-                        iteratingArray({array, l, 
-                            item: p
-                        });
-                    }
-                    
-                }
-                
             });
-        } else { return; }
-
+        }
     } else {
-        array.forEach(el => {
-            newArr = [...newArr, el];
-            iteratingArray({array, l, 
-                item: el
-            });
+        array.forEach(item => {
+            newArr[i] = item;
+            i++;
+            iteratingArray(arr, item);
         });
     }
 };
-
-iteratingArray({
-    array: arr, 
-    l: arrLength
-});
+iteratingArray(arr);
 
 console.log(newArr);
-
-
-// newArr.forEach(itm => {
-//     const string = itm.replace(/\-/g, '');
-//     let x = 0;
-
-//     for (let i = 0; i < string.length; i++) {
-//         array.forEach(elem => {
-//             if (string[i] !== elem) {
-//                 x++;
-//             }
-//         });
-//     }
-//     if(x >= l) {
-//         y++;
-//     }
-// });
-
-// ---------------------RECURSIVE-METHOD---------------------
-
-// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
-// let newArr = [];
-
-// const iteratingArray = (array, item) => {
-//     if (item) {
-//         if (+item.replace(/\-/g, '').length < arr.length) {
-//             array.forEach(el => {
-//                 const p = `${item}-${el}`;
-//                 newArr = [...newArr, p];
-//                 iteratingArray(array, p);
-//             });
-//         } else { return; }
-//     } else {
-//         array.forEach(el => {
-//             newArr = [...newArr, el];
-//             iteratingArray(array, el);
-//         });
-//     }
-// };
-// iteratingArray(arr);
-
-// console.log(newArr);
-
-// ---------------------NOT-RECURSIVE-METHOD---------------------
-
-// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
-// let newArr = [];
-
-// arr.forEach(a => {
-//     newArr = [...newArr, a];
-
-//     arr.forEach(b => {
-//         newArr = [...newArr, `${a}-${b}`];
-
-//         arr.forEach(c => {
-//             newArr = [...newArr, `${a}-${b}-${c}`];
-
-//             arr.forEach(d => {
-//                 newArr = [...newArr, `${a}-${b}-${c}-${d}`];
-
-//                 arr.forEach(e => {
-//                     newArr = [...newArr, `${a}-${b}-${c}-${d}-${e}`];
-
-//                     arr.forEach(f => {
-//                         newArr = [...newArr, `${a}-${b}-${c}-${d}-${e}-${f}`];    
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
-
-// console.log(newArr);
